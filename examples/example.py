969f99c2a8c4da43fe1cbcc54d429326c2f620a5
Dan Gordon <dan.gordon@anu.edu.au>
2024-01-05 09:42:08 +1100

    Work on README.md and example.py.

---
+++ examples/example.py
@@ -0,0 +1,25 @@
+import epyjson
+
+# Load in the network
+netw = epyjson.EJson.read_from_file(
+    '../tests/test_networks/netw_generic_a.json'
+)
+
+# Directly obtain and manipulate the underlying networkx graph. Normally, we
+# would only do this under special circumstances.
+nx_nodes = list(netw.graph.subgraph(['nd1', 'nd2']))
+
+# Obtain a list of components of type `Line`. Note that `components(...)` returns a generator
+# so it is often convenient to wrap it in `list(...)`.
+lines = list(netw.components(ctype='Line'))
+
+# Obtain raw e-JSON dict.
+raw = netw.raw_ejson()
+
+# Obtain connections from `ln2_3` to connected nodes.
+line_cons = list(netw.connections_from('ln2_3'))
+# Obtain data from first such connection.
+ln2_3_id, node_id, con_idx, con_data = line_cons[0]
+
+# Apply network reduction: telescope long strings of lines together, etc.
+epyjson.reduce_network(netw)
